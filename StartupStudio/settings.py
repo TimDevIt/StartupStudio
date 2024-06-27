386f24e9737a3a94f33609db0a01772ba858b1e3
TimDevIt <delootos1@mail.ru>
2024-06-27 15:31:10 +0300

    update: Change settings.py

--- StartupStudio/settings.py
+++ StartupStudio/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5hda8i)c_+-7vm^_odntxm@73q)c9=_r(t$j8o1w=a)(qdiwhv'
-
+#SECRET_KEY = 'django-insecure-5hda8i)c_+-7vm^_odntxm@73q)c9=_r(t$j8o1w=a)(qdiwhv'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-5hda8i)c_+-7vm^_odntxm@73q)c9=_r(t$j8o1w=a)(qdiwhv')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = []
 
 
